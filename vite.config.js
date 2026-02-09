ee758640e2bd691c742b9cf975823c9c807749ba
Stef <stefano-lev@protonmail.com>
2026-02-09 13:47:21 -0600

    Adjust vite.config to account for the subdomain routing

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 export default defineConfig({
-    plugins: [
-        react({
-            babel: {
-                plugins: [['babel-plugin-react-compiler']],
-            },
-        }),
-    ],
-    base: '/seiraji/',
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        },
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+  base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     }),
   ],
 
-  base: '/seiraji/',
+  base: '/',
 
   resolve: {
     alias: {
